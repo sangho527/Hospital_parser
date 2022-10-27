064d2c75031a7769b50ded0d5fbc2aac59e3e8c8
sangho527 <sy980527@naver.com>
2022-10-27 23:28:26 +0900

    ResponseEntity 적용 221027

--- src/main/java/com/springboot/hello1/controller/PutController.java
+++ src/main/java/com/springboot/hello1/controller/PutController.java
@@ -1,6 +1,8 @@
 package com.springboot.hello1.controller;
 
 import com.springboot.hello1.dto.MemberDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +14,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/put-api")
 
-//http:localhost:8080/api/v1/put-api/member
+//http://localhost:8080/api/v1/put-api/member
 public class PutController {
 
     @PutMapping(value = "/member")
@@ -26,16 +28,24 @@ public class PutController {
         return sb.toString();
     }
 
-    //http:localhost:8080/api/v1/put-api/member1
+    //http://localhost:8080/api/v1/put-api/member1
     @PutMapping(value = "/member1")
     public String postMemberDto1(@RequestBody MemberDto memberDto){
         return memberDto.toString();
     }
 
-    //http:localhost:8080/api/v1/put-api/member2
+    //http://localhost:8080/api/v1/put-api/member2
     @PutMapping(value = "/member2")
     public MemberDto postMemberDto2(@RequestBody MemberDto memberDto){
         return memberDto;
+    }
+
+    //http://localhost:8080/api/v1/put-api/member3
+    @PutMapping(value = "/member3")
+    public ResponseEntity<MemberDto> postMemberDto3(@RequestBody MemberDto memberDto){
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+                .body(memberDto);
     }
 
 
