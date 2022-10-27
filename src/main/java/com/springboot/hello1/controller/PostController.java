5ce2808b37da4a918c7a93e1bd31d674c65a2774
sangho527 <sy980527@naver.com>
2022-10-27 23:03:52 +0900

    DTO 객체 타입 적용 221027

--- src/main/java/com/springboot/hello1/controller/PostController.java
+++ src/main/java/com/springboot/hello1/controller/PostController.java
@@ -1,8 +1,9 @@
 package com.springboot.hello1.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.springboot.hello1.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
@@ -11,5 +12,21 @@ public class PostController {
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample() {
         return "Hello Post API";
+    }
+
+    @PostMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> postData){
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map ->{
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PostMapping(value = "/member2")
+    public String postMemberDto(@RequestBody MemberDto memberDto){
+        return memberDto.toString();
     }
 }
---
+++ src/main/java/com/springboot/hello1/controller/PutController.java
@@ -0,0 +1,43 @@
+package com.springboot.hello1.controller;
+
+import com.springboot.hello1.dto.MemberDto;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.lang.reflect.Member;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/put-api")
+
+//http:localhost:8080/api/v1/put-api/member
+public class PutController {
+
+    @PutMapping(value = "/member")
+    public String postMember(@RequestBody Map<String, Object> putData) {
+        StringBuilder sb = new StringBuilder();
+
+        putData.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
+    //http:localhost:8080/api/v1/put-api/member1
+    @PutMapping(value = "/member1")
+    public String postMemberDto1(@RequestBody MemberDto memberDto){
+        return memberDto.toString();
+    }
+
+    //http:localhost:8080/api/v1/put-api/member2
+    @PutMapping(value = "/member2")
+    public MemberDto postMemberDto2(@RequestBody MemberDto memberDto){
+        return memberDto;
+    }
+
+
+
+}
