89914a57716c41be17008ee1111dd3d724fc9420
sangho527 <sy980527@naver.com>
2022-10-27 15:35:09 +0900

    postController 적용 221027

--- src/main/java/com/springboot/hello1/controller/GetController.java
+++ src/main/java/com/springboot/hello1/controller/GetController.java
@@ -4,9 +4,9 @@ import com.springboot.hello1.dto.MemberDto;
 import org.springframework.web.bind.annotation.GetMapping;
 
 public class GetController {
-
     @GetMapping(value="/request3")
     public String getRequestParam3(MemberDto memberDto){
+
         return memberDto.toString();
     }
 }
