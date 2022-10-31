55d375048161dd15c5327ee91b039a9ec7cdc6b0
sangho527 <sy980527@naver.com>
2022-10-31 15:46:43 +0900

    Test ignored 에러 해결중 221031

--- src/main/java/parser/ReadLineContext.java
+++ src/main/java/parser/ReadLineContext.java
@@ -14,7 +14,7 @@ public class ReadLineContext<T> {
         this.parser = parser;
     }
 
-    public List<T> readByLine(String filename) throws IOException, FileNotFoundException {
+    public List<T> readByLine(String filename) throws IOException{
         // 삽
         List<T> result = new ArrayList<>();
         BufferedReader reader = new BufferedReader(
@@ -22,7 +22,12 @@ public class ReadLineContext<T> {
         );
         String str;
         while ((str = reader.readLine()) != null) {
-            result.add(parser.parse(str));
+            try {
+                result.add(parser.parse(str));
+            }
+            catch (Exception e){
+                System.out.printf("파싱중 문제가 생겨 이 라인은 넘어갑니다. 파일내용:%s\n", str.substring(0,50));
+            }
         }
         reader.close();
         return result;
--- src/test/java/parser/HospitalParserTest.java
+++ src/test/java/parser/HospitalParserTest.java
@@ -3,15 +3,36 @@ package parser;
 import com.springboot.hello1.domain.Hospital;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class HospitalParserTest {
 
 
     String line1 = "\"2\",\"의원\",\"01_01_02_P\",\"3620000\",\"PHMA119993620020041100005\",\"199907071\",\"영업/정상\",\"13\",\"영업중\",\"062-574-2802\",\"500867\",\"광주광역시 북구 일곡동 821번지 1호 2층	광주광역시 북구 설죽로 518, 2층 (일곡동)\",\"61041\",\"일곡부부치과의원\",\"2.01709E+13\",\"59:59.0\",\"치과의원\",\"190646.7771\",\"189589.4279\",\"치과의원\",\"2\",\"0\",\"0\",\"200\",\"401\",\"치과\",\"0\",\"0\",\"0\"";
+
+    @Autowired
+    ReadLineContext<Hospital> hospitalReadLineContext;
+
+    @Test
+    void oneHundreadThousandRows() throws IOException {
+        String filename = "C:\\Users\\PC\\Downloads\\fulldata_01_01_02_P_의원.csv";
+        List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
+        assertTrue(hospitalList.size() > 1000);
+        assertTrue(hospitalList.size() > 10000);
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(hospitalList.get(i).getHospitalName());
+        }
+        System.out.printf("파싱된 데이터 개수:", hospitalList.size());
+    }
 
     @Test
     @DisplayName("csv 1줄을 Hostpital로 잘 만드는지 Test")
