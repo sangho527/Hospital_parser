8cfe2b63387cbf60a1331e4b5eb8a06c09c3a761
sangho527 <sy980527@naver.com>
2022-11-03 11:43:13 +0900

    HospitalService 병렬처리 수정 221103

--- src/main/java/com/springboot/hello1/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello1/dao/HospitalDao.java
@@ -40,16 +40,16 @@ public class HospitalDao {
     }
 
     public int getCount() {
-        String sql = "select count(id) from nation_wide_hospitals1;";
+        String sql = "select count(id) from nation_wide_hospitals;";
         return this.jdbcTemplate.queryForObject(sql, Integer.class);
     }
 
     public void deleteAll() {
-        this.jdbcTemplate.update("delete from nation_wide_hospitals1");
+        this.jdbcTemplate.update("delete from nation_wide_hospitals");
     }
 
     public void add(Hospital hospital) {
-        String sql = "INSERT INTO `sh-db`.`nation_wide_hospitals1` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`)" +
+        String sql = "INSERT INTO `sh-db`.`nation_wide_hospitals` (`id`, `open_service_name`, `open_local_government_code`, `management_number`, `license_date`, `business_status`, `business_status_code`, `phone`, `full_address`, `road_name_address`, `hospital_name`, `business_type_name`, `healthcare_provider_count`, `patient_room_count`, `total_number_of_beds`, `total_area_size`)" +
                 " VALUES (?,?,?," +
                 " ?,?,?," +
                 " ?,?,?," +
--- src/main/java/com/springboot/hello1/service/HospitalService.java
+++ src/main/java/com/springboot/hello1/service/HospitalService.java
@@ -4,16 +4,15 @@ import com.springboot.hello1.dao.HospitalDao;
 import com.springboot.hello1.domain.Hospital;
 import com.springboot.hello1.parser.ReadLineContext;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class HospitalService {
 
     private final ReadLineContext<Hospital> hospitalReadLineContext;
-
     private final HospitalDao hospitalDao;
 
     public HospitalService(ReadLineContext<Hospital> hospitalReadLineContext, HospitalDao hospitalDao) {
@@ -21,24 +20,71 @@ public class HospitalService {
         this.hospitalDao = hospitalDao;
     }
 
-    @Transactional
     public int insertLargeVolumeHospitalData(String filename) {
-        int cnt = 0;
+        List<Hospital> hospitalList;
         try {
-            List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
+            hospitalList = hospitalReadLineContext.readByLine(filename);
             System.out.println("파싱이 끝났습니다.");
-            for (Hospital hospital : hospitalList) { // loop구간
-                try {
-                    this.hospitalDao.add(hospital); // db에 insert하는 구간
-                    cnt++;
-                } catch (Exception e) {
-                    System.out.printf("id:%d 레코드에 문제가 있습니다.",hospital.getId());
-                    throw new RuntimeException(e);
-                }
-            }
+            hospitalList.stream()
+                    .forEach(hospital -> {
+                        try {
+                            this.hospitalDao.add(hospital); // db에 insert하는 구간
+                        } catch (Exception e) {
+                            System.out.printf("id:%d 레코드에 문제가 있습니다.\n",hospital.getId());
+                            throw new RuntimeException(e);
+                        }
+                    });
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        return cnt;
+        if (!Optional.of(hospitalList).isEmpty()) {
+            return hospitalList.size();
+        } else {
+            return 0;
+        }
     }
 }
+
+//
+//import com.springboot.hello1.dao.HospitalDao;
+//import com.springboot.hello1.domain.Hospital;
+//import com.springboot.hello1.parser.ReadLineContext;
+//import org.springframework.stereotype.Service;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//import java.io.IOException;
+//import java.util.List;
+//
+//@Service
+//public class HospitalService {
+//
+//    private final ReadLineContext<Hospital> hospitalReadLineContext;
+//
+//    private final HospitalDao hospitalDao;
+//
+//    public HospitalService(ReadLineContext<Hospital> hospitalReadLineContext, HospitalDao hospitalDao) {
+//        this.hospitalReadLineContext = hospitalReadLineContext;
+//        this.hospitalDao = hospitalDao;
+//    }
+//
+//    @Transactional
+//    public int insertLargeVolumeHospitalData(String filename) {
+//        int cnt = 0;
+//        try {
+//            List<Hospital> hospitalList = hospitalReadLineContext.readByLine(filename);
+//            System.out.println("파싱이 끝났습니다.");
+//            for (Hospital hospital : hospitalList) { // loop구간
+//                try {
+//                    this.hospitalDao.add(hospital); // db에 insert하는 구간
+//                    cnt++;
+//                } catch (Exception e) {
+//                    System.out.printf("id:%d 레코드에 문제가 있습니다.",hospital.getId());
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//        return cnt;
+//    }
+//}
